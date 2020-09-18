751560d7a134b122fbba36166d0d2818d3e319fd
Ted Koomen <koomen.ted@gmail.com>
2020-09-18 15:36:35 -0400

    commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,17 +15,6 @@ import "./layout.scss";
 const Layout = ({ children, path, navWhite }) => {
   return (
     <>
-    <Helmet>
-    <script>
-{`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-168019975-1', 'auto');
-ga('send', 'pageview');`}
-</script>
-    </Helmet>
       <div
         className="wrapper"
         style={{ margin: "auto", marginBottom: "-50px" }}
