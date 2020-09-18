bcfbe88f1c1fb6e3b0d531dea5bd92e5d7df8c29
Ted Koomen <koomen.ted@gmail.com>
2020-09-18 14:34:29 -0400

    add utag

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,24 +5,44 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Header from '../Header/Header'
-import './layout.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../Header/Header";
+import { Helmet } from "react-helmet";
+import "./layout.scss";
 
-const Layout = ({ children, path,navWhite }) => {
+const Layout = ({ children, path, navWhite }) => {
   return (
     <>
-      <div className="wrapper" style={{margin: 'auto', marginBottom: '-50px'}}>
-        <Header white={navWhite ? true : false } path={path}/>
+      <Helmet>
+        <head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-168019975-1"
+          ></script>
+          <script>
+            {`
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-168019975-1');
+  `}
+          </script>
+        </head>
+      </Helmet>
+      <div
+        className="wrapper"
+        style={{ margin: "auto", marginBottom: "-50px" }}
+      >
+        <Header white={navWhite ? true : false} path={path} />
         <main>{children}</main>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
